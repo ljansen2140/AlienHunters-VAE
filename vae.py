2ccf9c3070300d1959b118fe8b176aca1413f374
ysvar <66091297+ysvar@users.noreply.github.com>
2021-02-15 16:09:10 -0600

    Update vae.py

--- vae.py
+++ vae.py
@@ -142,68 +142,41 @@ class VAE(keras.Model):
 
 vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam())
-vae.fit(pic_data, epochs=30, batch_size=128)
-
-"""
-## Display a grid of sampled digits
-"""
-
-import matplotlib.pyplot as plt
 
 
-def plot_latent_space(vae, n=30, figsize=15):
-    # display a n*n 2D manifold of digits
-    digit_size = 28
-    scale = 1.0
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-scale, scale, n)
-    grid_y = np.linspace(-scale, scale, n)[::-1]
+max_epochs = 5
+rows = 4 # defining no. of rows in figure
+cols = 12 # defining no. of colums in figure
+f = plt.figure(figsize=(2*cols,2*rows)) 
+f.tight_layout()
 
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = vae.decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[
-                i * digit_size : (i + 1) * digit_size,
-                j * digit_size : (j + 1) * digit_size,
-            ] = digit
+for epoch in range(max_epochs):
+    history = vae.fit(pic_data, epochs=1, batch_size=128)
+    f.add_subplot(rows,cols, epoch+1)
+    plt.imshow()  #Something goes between the parentheses, but I can't figure out what
 
-    plt.figure(figsize=(figsize, figsize))
-    start_range = digit_size // 2
-    end_range = n * digit_size + start_range
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap="Greys_r")
-    plt.show()
+plt.savefig("results.png")
 
 
-plot_latent_space(vae)
+print(history.history.keys())
 
-"""
-## Display how the latent space clusters different digit classes
-"""
-
-
-def plot_label_clusters(vae, data, labels):
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = vae.encoder.predict(data)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=labels)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.show()
+# plot of the summary for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['reconstruction_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 
-(x_train, y_train), _ = keras.datasets.mnist.load_data()
-x_train = np.expand_dims(x_train, -1).astype("float32") / 255
 
-plot_label_clusters(vae, x_train, y_train)
+
+
+
+
+
+
+
+
+
